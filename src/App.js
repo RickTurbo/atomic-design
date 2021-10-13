f80fe7147260792e7bb37dc04228f6181dec6e2d
RickTurbo <73983715+RickTurbo@users.noreply.github.com>
2021-10-13 21:58:43 +0900

    Organism

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
 import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
 import { SerchInput } from "./components/molecules/SearchInput";
+import { UseCard } from "./components/organisms/user/UserCard";
 import "./styles.css";
+
+const user = {
+  name: "ターボ",
+  image: "https://source.unsplash.com/cfIcTiopin4",
+  email: "1234@example.com",
+  phone: "090-1111-2222",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
@@ -10,6 +22,7 @@ export default function App() {
       <SecondaryButton>検索</SecondaryButton>
       <br />
       <SerchInput />
+      <UseCard user={user} />
     </div>
   );
 }
